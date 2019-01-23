d353fc90d9b850f2b6bdd9de1c4681aa07f9787b
Fedya Tsarenko <44132297+Skybay2015@users.noreply.github.com>
2019-01-23 23:06:49 +0200

    new commit

--- js.js
+++ js.js
@@ -5,7 +5,9 @@ jQuery(function ($) {
     let add = $('.add');
 
     function newTodo (value) {
-        todoes.push (value);
+        todoes.push({text: value,
+                    completed: false,
+                    editable: false});
     }
 
     function renderTodo () {
@@ -13,33 +15,46 @@ jQuery(function ($) {
         $.each(todoes, function (i) {
             todoList.append(`
             <li>
-                <p class="info">${this}</p>
+                <p class="info" data-index="${i}">${this.text}</p>
+                <input type="text"  ${this.editable ? "" : "hidden"} class="edit-input" data-index="${i}">
                 <button class="delete" data-index="${i}">delete</button>  
                 <div>
-                  <input type="checkbox" class="check" data-index="${i}">
-                  <div  class="done" data-index="${i}" hidden>
+                 <input type="checkbox" ${this.completed ? "hidden" : ""} class="check" data-index="${i}">
+                  <div  class="done" ${this.completed ? "hidden" : ""} data-index="${i}">
                       <i class="fas fa-check"></i>
                   </div>
-                  </div>
-                <button class="edit">Edit</button>
+                 </div>
+                <button class="edit" data-index="${i}">Edit</button>
             </li>
             `);
         });
     }
 
-    function deleteTodo (index) {
+    function deleteTodo(index) {
         todoes.splice(index, 1);
         renderTodo();
+    }
+
+    function doneTodo(value) {
+        let index = $(value).data('index');
+        todoes[index].completed = true;
+        addIcon(value, index);
+    }
+
+    function addIcon(value, index) {
+        if (todoes[index].completed = true) {
+            $(value).siblings().prop('hidden', false);
+        }
     }
 
 
 
     input.on('keydown', function (e) {
-        if (e.keyCode == 13)  {
+        if (e.keyCode == 13) {
             newTodo(this.value);
             renderTodo();
             this.value = '';
-         }
+        }
     });
 
     add.on('click', function () {
@@ -55,14 +70,45 @@ jQuery(function ($) {
 
     $('#todo').on('click', '.check', function () {
         $(this).prop('hidden', true);
-        $(this).siblings().prop('hidden', false);
+        doneTodo(this);
     });
 
-    $('#todo').on('click', '.edit', function(){
-        $(this).siblings(".info").prop('contenteditable', true);
-        $(this).siblings(".info").on('change'), function f() {
-            $('.info').prop('contenteditable', false);
-        }
+    $('#todo').on('click', '.edit', function () {
+        beginEdit(this, '.edit-input');
+        endEdit(this, '.edit-input');
     });
+
+    function beginEdit(value, sibling) {
+        let index = $(value).data('index');
+        todoes[index].editable = true;
+        if (todoes[index].editable = true) {
+            $(value).siblings(sibling).prop('hidden', false);
+        }
+
+    }
+
+    function endEdit(value, sibling) {
+        $(value).siblings(sibling).on('keydown', function (e) {
+            if (e.keyCode == 13) {
+                saveEdit(value);
+                $(sibling).prop('hidden', true);
+                $(sibling)[0].value = "";
+            }
+        });
+    }
+
+    function saveEdit(sibling) {
+        saveTodo(sibling);
+        console.log(sibling.value);
+    }
+
+    function saveTodo (sibling) {
+        let index = $(sibling).data('index');
+        todoes[index].text = $('.edit-input')[0].value;
+        console.log(todoes[index]);
+        todoes[index].editable = false;
+        renderTodo();
+    }
+
 
 });
