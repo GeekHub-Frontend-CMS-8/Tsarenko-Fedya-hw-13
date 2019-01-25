c966f143780d24a91c9ed428fdfcabe2e26a7014
Fedya Tsarenko <44132297+Skybay2015@users.noreply.github.com>
2019-01-25 18:05:39 +0200

    end of the work

--- js.js
+++ js.js
@@ -3,31 +3,40 @@ jQuery(function ($) {
     let todoList = $('.todoes');
     let input = $('.add-todo');
     let add = $('.add');
+    let more = $('#load-more');
 
-    function newTodo (value) {
-        todoes.push({text: value,
-                    completed: false,
-                    editable: false});
+    function newTodo(value) {
+        todoes.push({
+            title: value,
+            completed: false,
+            editable: false
+        });
     }
 
-    function renderTodo () {
+    function renderTodo() {
         todoList.empty();
         $.each(todoes, function (i) {
-            todoList.append(`
-            <li>
-                <p class="info" data-index="${i}">${this.text}</p>
+            todoList.prepend(`
+            <li style="display: flex; 
+                       align-items: center">
+                <p class="info" data-index="${i}">${this.title}</p>
                 <input type="text"  ${this.editable ? "" : "hidden"} class="edit-input" data-index="${i}">
                 <button class="delete" data-index="${i}">delete</button>  
-                <div>
+                <div style="display: inline-block">
                  <input type="checkbox" ${this.completed ? "hidden" : ""} class="check" data-index="${i}">
-                  <div  class="done" ${this.completed ? "hidden" : ""} data-index="${i}">
+                  <div  class="done" ${this.completed ? "" : "hidden"} data-index="${i}">
                       <i class="fas fa-check"></i>
                   </div>
                  </div>
                 <button class="edit" data-index="${i}">Edit</button>
             </li>
             `);
+            saveTodoes();
         });
+    }
+
+    function saveTodoes() {
+        localStorage.setItem('todoList', JSON.stringify(todoes));
     }
 
     function deleteTodo(index) {
@@ -46,7 +55,6 @@ jQuery(function ($) {
             $(value).siblings().prop('hidden', false);
         }
     }
-
 
 
     input.on('keydown', function (e) {
@@ -75,10 +83,11 @@ jQuery(function ($) {
 
     $('#todo').on('click', '.edit', function () {
         beginEdit(this, '.edit-input');
-        endEdit(this, '.edit-input');
+        let index = $(this).data('index');
+        endEdit(this, '.edit-input', index);
     });
 
-    function beginEdit(value, sibling) {
+    function beginEdit(value, sibling,) {
         let index = $(value).data('index');
         todoes[index].editable = true;
         if (todoes[index].editable = true) {
@@ -87,28 +96,45 @@ jQuery(function ($) {
 
     }
 
-    function endEdit(value, sibling) {
-        $(value).siblings(sibling).on('keydown', function (e) {
+    function endEdit(value, element, index) {
+        $(value).siblings(element).on('keydown', function (e) {
             if (e.keyCode == 13) {
-                saveEdit(value);
-                $(sibling).prop('hidden', true);
-                $(sibling)[0].value = "";
+                saveEdit(value, element, index);
+                $(element).prop('hidden', true);
+                $(element)[0].value = "";
             }
         });
     }
 
-    function saveEdit(sibling) {
-        saveTodo(sibling);
-        console.log(sibling.value);
-    }
 
-    function saveTodo (sibling) {
-        let index = $(sibling).data('index');
-        todoes[index].text = $('.edit-input')[0].value;
-        console.log(todoes[index]);
+    function saveEdit(value, element, index) {
+        todoes[index].text = $(value).siblings(element)[0].value;
         todoes[index].editable = false;
         renderTodo();
     }
 
+    $(window).on('load', function () {
+        if (localStorage.getItem('todoList') != undefined) {
+            todoes = JSON.parse(localStorage.getItem('todoList'));
+            renderTodo()
+        }
+    });
 
+    more.on('click', function () {
+        let moreTodoes = new XMLHttpRequest();
+        moreTodoes.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+        moreTodoes.onload = function () {
+            let todoesData = JSON.parse(moreTodoes.responseText);
+            addNewHtmlTodoes(todoesData)
+        }
+        moreTodoes.send();
+    });
+
+    function addNewHtmlTodoes(data) {
+        for (i = 0; i <= 10; i++) {
+            data[i].editable = false;
+            todoes.push(data[i]);
+        }
+        renderTodo();
+    }
 });
